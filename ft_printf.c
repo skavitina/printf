61dcf0aec20c8b740dbc6586ee9d8a5d4a6b1948
Trough Peggie <tpeggie@oa-a2.msk.21-school.ru>
2022-01-21 22:51:15 +0300

    21.01.2022

--- ft_printf.c
+++ ft_printf.c
@@ -31,18 +31,18 @@ int	ft_putstr_fd(char *s, int fd)
 	return (len);
 }
 
-int	converse_arg(char flag, va_list arg)
+int	converse_arg(char flag, va_list arg)// в va_arg указываем какой тип аргументов мы хотим получить
 {
 	int	length;
 
 	length = 0;
-	if (flag == 'c')
-		length += ft_putchar_fd(va_arg(arg, int), 1);
-	else if (flag == 's')
-		length += ft_putstr_fd(va_arg(arg, char *), 1);
-	else if (flag == 'p')
+	if (flag == 'c')// печать 1 символа
+		length += ft_putchar_fd(va_arg(arg, int), 1);//указатель на список аргументов и тип переменной который хотим получить 
+	else if (flag == 's')//печать строки
+		length += ft_putstr_fd(va_arg(arg, char *), 1);// указатель на список аргументов и тип переменной который хотим получить 
+	else if (flag == 'p')//Аргумент указателя *void должен быть напечатан в шестнадцатиричном формате
 		length += ft_putstr_fd("0x", 1) + ft_putnbr_pointer(
-				va_arg(arg, unsigned long int));
+				va_arg(arg, unsigned long int));//литерал для указателя. unsigned long потому что указатель это 4 байт - вырабатывается в соответствии с 4х байтовой системой 
 	else if (flag == 'd')
 		length += ft_putnbr_decimal(va_arg(arg, int));
 	else if (flag == 'i')
@@ -56,22 +56,22 @@ int	converse_arg(char flag, va_list arg)
 	return (length);
 }
 
-int	ft_printf(const char *format, ...)// format - последний аргумент известного размера 
+int	ft_printf(const char *format, ...)// функция с переменным количеством переменных. Для определения параметров неопределенной длины в таких функциях используется многоточие
 {
-	va_list	argptr; //инициализация списка для получения указателя argptr
-	int		length;// количество символов, которые видели
+	va_list	argptr; // тип va_list опеределен в файле stdarg.h. . Позволяет обрабатывать списки параметров с нефиксированным количеством
+	int		length;// количество символов, которые печатать буду
 
-	va_start(argptr, format);
+	va_start(argptr, format);// Получаем указатель на начало списка аргументов, format - последний аргумент известного размера
 	length = 0;
-	while (*format)
+	while (*format)//посимвольно иду по аргументам
 	{
-		if (*format != '%')// если знака процента нет,значит посимвольно выводим аргументы 
+		if (*format != '%')// если аргумент не равен знаку процента ,значит посимвольно выводим аргументы 
 			length += ft_putchar_fd(*format, 1); // посимвольная печать значений format
 		else
 		{	
-			format++;
+			format++;// если все-таки встречаем знак процента -> то пропускаем его и смотрим на следующий символ
 			if (*format == '%') // если 2% подряд то выводим процент
-				length += ft_putchar_fd(*format, 1);
+				length += ft_putchar_fd(*format, 1);//подсчет количества аргументов посимвольно
 			else
 				length += converse_arg(*format, argptr);// если что-то другое,то переходим в функцию converse arg - то есть преобразование аргумента
 		}
